8f33a21c777502792cb260d9e52e1322d4af4a7d
Matt Carroll <oholiab@gmail.com>
2012-07-03 23:36:38 +0100

    Added Text class
    Basic animated text class which will print out a string at a given rate.
    Next will be to assign any one text entity to a character so they can
    say their lines, and add restrictions to the length and wrapping of the
    line based on position and screen width. Maybe make it return a bool for
    when it is and isn't updating so it can block other actions.

--- God Damn/src/com/prontab/goddamn/GodDamn.java
+++ God Damn/src/com/prontab/goddamn/GodDamn.java
@@ -21,31 +21,40 @@ public class GodDamn extends BasicGame {
 }
   
   Matt matt;
+  Text text;
   
   public void init(GameContainer c) throws SlickException {
     matt = new Matt(50, 50);
-    
+    text = new Text("", 220, 80, 70);
   }
   
   public void render(GameContainer c, Graphics g) throws SlickException {
     g.setColor(Color.white);
     g.fill(new Rectangle(0, 0, 640, 480));
     matt.draw();
+    text.draw(g);
   }
 
-  public void update(GameContainer c, int delta) throws SlickException {
+  public void update(GameContainer c, int delta) throws SlickException{
+    handleinput(c);
+    text.update(delta);
+  }
+  private void handleinput(GameContainer c) throws SlickException{
     Input input = c.getInput();
     if (input.isKeyDown(Input.KEY_ESCAPE)) {
       c.exit();
     }
     if (input.isKeyDown(Input.KEY_S)) {
       matt.stand();
+      text.reset();
     }
     if (input.isKeyDown(Input.KEY_Q)) {
       matt.shrug();
+      text.settext("I dunno");
     }
     if (input.isKeyDown(Input.KEY_T)) {
       matt.talk();
+      text.settext("Can we go get a drink now?");
     }
   }
 }
---
+++ God Damn/src/com/prontab/goddamn/Text.java
@@ -0,0 +1,61 @@
+package com.prontab.goddamn;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+
+public class Text {
+  
+  String printline, line;
+  int posx, posy, updatefreq, length;
+  int time, lineindex;
+  
+  public Text(String text, int x, int y, int freq){
+    reset();
+    posx = x;
+    posy = y;
+    updatefreq = freq;
+    if (text.length() == 0){
+      return;
+    }
+    else {
+      line = text;
+      return;
+    }
+  }
+  
+  public void settext(String text){
+    reset();
+    line = text;
+  }
+  
+  public void setprinttext(String text){
+    reset();
+    printline = text;
+  }
+  
+  public void reset(){
+    lineindex = 0;
+    line = "";
+    printline = "";
+    length = 0;
+    time = 0;
+  }
+  
+  public void update(int delta){
+    if(printline.length() == line.length()){
+      return;
+    }
+    time += delta;
+    if (time >= updatefreq){
+      time = 0;
+      printline = printline + line.charAt(lineindex);
+      lineindex++;
+    }
+  }
+  
+  public void draw(Graphics g){
+    g.setColor(Color.black);
+    g.drawString(printline, posx, posy);
+  }
+
+}
